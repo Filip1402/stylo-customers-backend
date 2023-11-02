c05f0cbd2eff04eb2eeac557a2a5eec6a2e17dd0
robert4361 <robert4361@gmail.com>
2023-11-02 21:41:37 +0100

    Return customer instead of success message

--- src/controllers/CustomersController.js
+++ src/controllers/CustomersController.js
@@ -21,22 +21,18 @@ async function login(req, res) {
   try {
     const response = await service.loginCustomer(email, password);
     const customer = response.customer;
-    if (customer.isEmailVerified == false) {
-      return res.status(401).json({ Error: "Email address not verified" });
-    } else {
-      return res.status(200).json({ Success: "Valid customer credentials" });
-    }
+    return res.status(200).json({ customer });
   } catch (err) {
     return res
       .status(401)
-      .json({ Error: "Invalid credentials or account doesn't exist" });
+      .json({ error: "Invalid credentials or account doesn't exist" });
   }
 }
 
 async function signup(req, res) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ Error: errors.array()[0].msg });
+    return res.status(400).json({ error: errors.array()[0].msg });
   }
 
   const { first_name, last_name, email, password, phone_number } = req.body;
